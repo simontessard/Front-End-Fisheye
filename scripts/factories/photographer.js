da74c8d44c5e840daec709e21307a8a7b517d98b
Simon Tessard <simon.tessard@gmail.com>
2022-05-04 12:22:14 +0200

    Added location, quote and pricing to photographers cards

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const location = document.createElement( 'p' );
+        location.textContent = city + ', ' + country;
+
+        const quote = document.createElement( 'p' );
+        quote.textContent = tagline;
+
+        const pricing = document.createElement( 'p' );
+        pricing.textContent = price + '€/jour';
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(quote);
+        article.appendChild(pricing);
         return (article);
     }
     return { name, picture, getUserCardDOM }
