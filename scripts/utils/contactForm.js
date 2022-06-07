9704f82c32a7bd163eab6e60df2a460b6f40bccd
Simon Tessard <simon.tessard@gmail.com>
2022-06-07 16:59:32 +0200

    Not fully working version of focus on contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,8 @@
+
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 function closeModal() {
@@ -25,25 +27,59 @@ function displayInputs() {
     })
 
     const nomLabel = document.createElement('label');
-    nomLabel.textContent='Nom';
+    nomLabel.textContent = 'Nom';
     const nomInput = document.createElement('input');
 
     const emailLabel = document.createElement('label');
-    emailLabel.textContent='Email';
+    emailLabel.textContent = 'Email';
     const emailInput = document.createElement('input');
 
     const messageLabel = document.createElement('label');
-    messageLabel.textContent='Votre message';
+    messageLabel.textContent = 'Votre message';
     const messageInput = document.createElement('input');
     messageInput.style.height = '200px';
 
     form.append(nomLabel, nomInput, emailLabel, emailInput, messageLabel, messageInput);
+
+    addFocus();
 }
 
+function addFocus() {
+    // add all the elements inside modal which you want to make focusable
+    const focusableElements = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+    const modal = document.querySelector('#contact_modal'); // select the modal by it's id
+
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+    const focusableContent = modal.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+    document.addEventListener('keydown', function (e) {
+        let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+        if (!isTabPressed) {
+            return;
+        }
+
+        if (e.shiftKey) { // if shift key pressed for shift + tab combination
+            if (document.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus(); // add focus for the last focusable element
+                e.preventDefault();
+            }
+        } else { // if tab key is pressed
+            if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+                firstFocusableElement.focus(); // add focus for the first focusable element
+                e.preventDefault();
+            }
+        }
+    });
+
+    firstFocusableElement.focus();
+    document.activeElement.blur();
+}
 /* document.onreadystatechange = function () {
-	if (document.readyState === 'interactive') {
-		displayInputs();
-	}
+    if (document.readyState === 'interactive') {
+        displayInputs();
+    }
 } */
 
 export { displayModal, closeModal, displayInputs }
