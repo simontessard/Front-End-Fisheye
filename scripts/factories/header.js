2ff9910710bcf424fbc5e8fea24eb83fb7b446b4
Simon Tessard <simon.tessard@gmail.com>
2022-06-16 16:33:51 +0200

    eslint fixes

--- scripts/factories/header.js
+++ scripts/factories/header.js
@@ -1,22 +1,22 @@
-function headerFactory() {
-    // Ajout d'un lien vers l'accueil sur le logo
-    const logo = document.querySelector(".logo");
-    const titre = document.querySelector("header h1");
-    const header = document.querySelector('header');
-    const link = document.createElement('a');
-    const nav = document.createElement('nav');
-    
-    header.insertBefore(link, logo);
-    header.appendChild(nav);
-    
-    logo.setAttribute('alt', 'Fisheye logo - Page accueil');
-    link.setAttribute('tabindex', 0);
-    link.setAttribute('href', 'index.html');
-    link.appendChild(logo);
-    nav.appendChild(link);
+function headerFactory () {
+  // Ajout d'un lien vers l'accueil sur le logo
+  const logo = document.querySelector('.logo')
+  const titre = document.querySelector('header h1')
+  const header = document.querySelector('header')
+  const link = document.createElement('a')
+  const nav = document.createElement('nav')
 
-    if (titre != null) {
-        titre.setAttribute('tabindex', 0);
-    }
-} 
+  header.insertBefore(link, logo)
+  header.appendChild(nav)
+
+  logo.setAttribute('alt', 'Fisheye logo - Page accueil')
+  link.setAttribute('tabindex', 0)
+  link.setAttribute('href', 'index.html')
+  link.appendChild(logo)
+  nav.appendChild(link)
+
+  if (titre != null) {
+    titre.setAttribute('tabindex', 0)
+  }
+}
 export { headerFactory }
