985117234c0603f54fc9b1e51b4f01590ae335d7
Simon Tessard <simon.tessard@gmail.com>
2022-06-19 18:27:49 +0200

    Variables fixes

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,18 +1,8 @@
 /* eslint-disable no-undef */
-// Ajout d'un lien vers l'accueil sur le logo
-const logo = document.querySelector('.logo')
-const titre = document.querySelector('header h1')
-const header = document.querySelector('header')
-const link = document.createElement('a')
-const nav = document.createElement('nav')
-header.insertBefore(link, logo)
-header.appendChild(nav)
-titre.setAttribute('tabindex', 0)
-logo.setAttribute('alt', 'Fisheye logo - Page accueil')
-link.setAttribute('tabindex', 0)
-link.setAttribute('href', 'index.html')
-link.appendChild(logo)
-nav.append(link, titre)
+import { headerFactory } from '../factories/header.js'
+import { photographerFactory } from '../factories/photographer.js'
+
+headerFactory('accueil')
 
 async function getPhotographers () {
   // Données récupérées grâce au fichier JSON
@@ -39,7 +29,7 @@ async function displayData (photographers) {
       // Désigne le second enfant de la carte, soit la div contenent les informations du photographe
       userCardDOM.children[1].setAttribute('tabindex', 0)
       // Assigner l'ID à la vignette du photographe
-      vignette = userCardDOM.children[0]
+      const vignette = userCardDOM.children[0]
       vignette.setAttribute('id', photographerModel.id)
       vignette.setAttribute('href', 'photographer.html?id=' + vignette.id)
 
